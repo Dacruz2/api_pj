760975723721ff3660e2f0a9a82c271fc6ab6537
Dacruz2 <ra53212580828@acaonsfatima.org.br>
2024-08-02 11:44:22 -0300

    atualização

--- api.js
+++ api.js
@@ -9,9 +9,9 @@ servidor.get('/calculadora/somar/:n1/:n2', (req, resp) => {
 })
 
 servidor.get('/calculadora/subtrair/:n1/:n2', (req, resp) =>{
-    let n1 = Number(req.params.n1)
+    let n3 = Number(req.params.n3)
     let n2 = Number(req.params.n2)
-    let subtrair = n1 - n2
+    let subtrair = n3 - n2
 
     resp.send({
         subtrair
