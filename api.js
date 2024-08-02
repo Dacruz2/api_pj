aab70e0827d8f274f618a5b6d86679db271c16ae
Dacruz2 <ra53212580828@acaonsfatima.org.br>
2024-08-02 11:39:17 -0300

    subtrair

--- api.js
+++ api.js
@@ -8,4 +8,15 @@ servidor.get('/calculadora/somar/:n1/:n2', (req, resp) => {
     resp.send("o resultado da soma é:" + soma)
 })
 
+servidor.get('/calculadora/subtrair/:n1/:n2', (req, resp) =>{
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+    let subtrair = n1 - n2
+
+    resp.send({
+        subtrair
+    })
+})
+
+
 servidor.listen(5050, ()=> console.log("API subiu na porta 5050"))
